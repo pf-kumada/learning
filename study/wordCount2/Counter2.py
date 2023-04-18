75b0e13a1be7947ee0f3f94dc6a4d862446d7f2d
pf-kumada <kumada@profire.co.jp>
2023-04-18 19:05:29 +0900

    集合は参照渡しのため、copyを使用

--- study/wordCount2/Counter2.py
+++ study/wordCount2/Counter2.py
@@ -3,7 +3,7 @@ import glob
 print("------------------------------------------------------------------------------------")
 
 # 指定したフォルダ内において、txtファイル一覧を取得
-path_list = glob.glob("C:/Users/user/Documents/learning/study/wordCount2/*.txt")
+path_list = glob.glob("C:/Users/user/Documents/learning/study/wordCount2/test*.txt")
 print(path_list)
 
 """
@@ -19,36 +19,43 @@ for file in path_list:
 file_word = {}
 tmp = ""
 file_set = set()
-
+file_first = set()
+file_escape = set()
 # フォルダ内のファイルを全て取得する(with使わない)
 for i in range(len(path_list)):
    with open(path_list[i], encoding="utf-8") as file:
     file_data = file.read()
-
-fs = file_set.add(path_list[i])
-
-
+    
+    file_set.add(path_list[i]) #返り値None(file_setの中には追加はされている)
+    # file_word[tmp][1].add(path_list[i])
 
 # ファイルの中身を1文字ずつ取り出す
     for char in file_data:
 # スペースだった場合、スペースまでのcharを1つの単語にして、file_wordのキーに設定し、値に1を加算する。
+# 各単語に対してどのファイルから吐き出されたものなのかそれぞれ辞書の値（リスト）の[1]要素目に出力する
         if(char == " " or char == "," or char == ":" or char == "\n" or char == "、" or char == "。" or char == ";" or char == "[" or char =="'"):
             if(tmp in file_word):
-                    file_word[tmp] = [file_word[tmp][0] + 1, file_word[tmp][1].add(path_list[i])]  
+                    file_word[tmp] = [file_word[tmp][0] + 1, file_set] 
 
             else:
-                    file_word[tmp] = [1, file_set[i]]
+                    file_first.add(path_list[i])
+                    file_escape = file_first.copy()
+                    file_word[tmp] = [1, file_escape]
             tmp = ""            
+            file_first.discard(path_list[i])
         else:
                 tmp += char        
-# ファイル単位で区切るようにする
+# 改行などが入っていない場合、ファイルが連結されてしまうのでファイル単位で区切るようにする
     if not tmp == "":
         if(tmp in file_word):
-            file_word[tmp] = [file_word[tmp][0] + 1, file_word[tmp][1].add(path_list[i])]
+            file_word[tmp] = [file_word[tmp][0] + 1, file_set]
             tmp = ""  
         else:
-            file_word[tmp] = [1, file_set.add(path_list[i])]
+            file_first.add(path_list[i])
+            file_escape = file_first.copy()
+            file_word[tmp] = [1, file_escape]
             tmp = ""
+            file_first.discard(path_list[i])
 
 # 辞書のキーをリストに取り出す
 file_keylist = list(file_word.keys())
@@ -85,4 +92,4 @@ while i < len(fv):
 for i in range(len(fk)):
 	print(fk[i] + ":" + str(fv[i]))
 
-# 各単語に対してどのファイルから吐き出されたものなのかそれぞれ出力する
+
