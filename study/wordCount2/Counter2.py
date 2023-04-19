f0b9db0827988b8dfa2d6ec879abf523328e634b
pf-kumada <kumada@profire.co.jp>
2023-04-19 18:53:40 +0900

    " "の文字コード確認、" "が連続している場合を考慮できていない

--- study/wordCount2/Counter2.py
+++ study/wordCount2/Counter2.py
@@ -3,39 +3,32 @@ import glob
 print("------------------------------------------------------------------------------------")
 
 # 指定したフォルダ内において、txtファイル一覧を取得
-path_list = glob.glob("C:/Users/user/Documents/learning/study/wordCount2/test*.txt")
+path_list = glob.glob("C:/Users/user/Documents/learning/study/wordCount2/*.txt")
 print(path_list)
 
-"""
-# 取得したファイル一覧を一行ずつ表示
-for file in path_list:
-    print(file)
-
-
-
-    file_word = dict(zip(char))
-    print(file_word)
-"""
 file_word = {}
 tmp = ""
 file_set = set()
 file_first = set()
 file_escape = set()
-# フォルダ内のファイルを全て取得する(with使わない)
+
+# フォルダ内のファイルを全て取得する
 for i in range(len(path_list)):
    with open(path_list[i], encoding="utf-8") as file:
     file_data = file.read()
-    
-    file_set.add(path_list[i]) #返り値None(file_setの中には追加はされている)
-    # file_word[tmp][1].add(path_list[i])
 
 # ファイルの中身を1文字ずつ取り出す
     for char in file_data:
+
 # スペースだった場合、スペースまでのcharを1つの単語にして、file_wordのキーに設定し、値に1を加算する。
 # 各単語に対してどのファイルから吐き出されたものなのかそれぞれ辞書の値（リスト）の[1]要素目に出力する
         if(char == " " or char == "," or char == ":" or char == "\n" or char == "、" or char == "。" or char == ";" or char == "[" or char =="'"):
             if(tmp in file_word):
-                    file_word[tmp] = [file_word[tmp][0] + 1, file_set] 
+                    file_word[tmp][1].add(path_list[i]) #返り値None(file_setの中には追加はされている)
+                    file_word[tmp][0] = file_word[tmp][0] + 1
+                    
+            elif(tmp == " "):
+                break
 
             else:
                     file_first.add(path_list[i])
@@ -45,10 +38,12 @@ for i in range(len(path_list)):
             file_first.discard(path_list[i])
         else:
                 tmp += char        
+
 # 改行などが入っていない場合、ファイルが連結されてしまうのでファイル単位で区切るようにする
     if not tmp == "":
         if(tmp in file_word):
-            file_word[tmp] = [file_word[tmp][0] + 1, file_set]
+            file_word[tmp][1].add(path_list[i])
+            file_word[tmp][0] = file_word[tmp][0] + 1
             tmp = ""  
         else:
             file_first.add(path_list[i])
@@ -61,11 +56,9 @@ for i in range(len(path_list)):
 file_keylist = list(file_word.keys())
 fk = file_keylist
 
-
 # 辞書の値ををリストに取り出す
 file_valuelist = list(file_word.values())
 fv = file_valuelist
-
 
 # 使用頻度の多い順にソートする
 # 要素番号の0番目とそれ以降を一つずつ比較し、先頭に最大の値が来るようにする
@@ -87,9 +80,8 @@ while i < len(fv):
 
     i += 1
 
-
 # 結果出力
 for i in range(len(fk)):
-	print(fk[i] + ":" + str(fv[i]))
-
-
+    print(fk[i] + ":" + str(fv[i]))
+    a = ord(fk[i])
+    print(a)
